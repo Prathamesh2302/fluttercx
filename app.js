7e92517f19ad4f1ad28d158c7c07a49212b8e1c4
Prathamesh2302 <prathamesh.mudme@searce.com>
2022-08-12 11:08:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const fetch = import("node-fetch");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const axios = require("axios");
+//require("dotenv").config();
+app.get('/', (req, res) => {
+    console.log("We are live");
+
+    res.status(200).send("We are live!!!");
+
+});
+app.post("/getResponse", express.json(), (req, res) => {
+    console.log(req);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
