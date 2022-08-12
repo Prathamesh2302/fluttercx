cad24734e748fe3f3fba4eb371a29c1e0c3d1c3b
Prathamesh2302 <prathamesh.mudme@searce.com>
2022-08-12 19:04:26 +0530

    editing post route

--- app.js
+++ app.js
@@ -3,16 +3,56 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const axios = require("axios");
-//require("dotenv").config();
+require("dotenv").config();
+
+
 app.get('/', (req, res) => {
     console.log("We are live");
+
 
     res.status(200).send("We are live!!!");
 
 });
 app.post("/getResponse", express.json(), (req, res) => {
-    console.log(req.body);
-    res.status(200).send({});
+    var query = req.body.title;
+    languageCode = "en";
+    const { SessionsClient } = require('@google-cloud/dialogflow-cx');
+    const client = new SessionsClient({ apiEndpoint: 'us-central1-dialogflow.googleapis.com' })
+
+    async function detectIntentText() {
+        const sessionId = Math.random().toString(36).substring(7);
+        const sessionPath = client.projectLocationAgentSessionPath(
+            process.env.projectId,
+            process.env.location,
+            process.env.agentId,
+            sessionId
+        );
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    languageCode,
+                    text: query,
+
+                },
+                languageCode,
+            },
+        };
+        const [response] = await client.detectIntent(request);
+        var data = "";
+        for (const message of response.queryResult.responseMessages) {
+            if (message.text) {
+                data += message.text.text;
+                console.log(`Agent Response: ${message.text.text}`);
+            }
+        }
+        console.log(data);
+        res.status(200).send(data);
+
+    }
+
+    detectIntentText();
+
 
 });
 
